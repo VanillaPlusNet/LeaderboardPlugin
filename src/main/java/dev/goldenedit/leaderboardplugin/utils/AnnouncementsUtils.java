a47b8965ec27b8e97c47e2f096b7c69644464c3c
GoldenEdit <sammyhmatthews@gmail.com>
2024-04-24 10:31:24 +0100

    Add broadcastMessage utility.

--- src/main/java/dev/goldenedit/leaderboardplugin/utils/AnnouncementsUtils.java
+++ src/main/java/dev/goldenedit/leaderboardplugin/utils/AnnouncementsUtils.java
@@ -1,4 +1,17 @@
 package dev.goldenedit.leaderboardplugin.utils;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+
 public class AnnouncementsUtils {
+    /**
+     * Broadcasts a message to all players on the server.
+     * @param message The message to broadcast.
+     */
+    public static void broadcastMessage(String message) {
+        String command = "bc " + message;
+        CommandSender console = Bukkit.getConsoleSender();
+        Bukkit.dispatchCommand(console, command);
+    }
+
 }
