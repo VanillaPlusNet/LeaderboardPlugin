d4367a6db70780193f023664d601dd6936e85d24
GoldenEdit <sammyhmatthews@gmail.com>
2024-04-25 11:15:21 +0100

    Max 3 kills per 6 hours of the same player to earn points.

--- src/main/java/dev/goldenedit/leaderboardplugin/listeners/DeathListener.java
+++ src/main/java/dev/goldenedit/leaderboardplugin/listeners/DeathListener.java
@@ -4,6 +4,7 @@ import dev.goldenedit.leaderboardplugin.LeaderboardPlugin;
 import dev.goldenedit.leaderboardplugin.PlayerKill;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -13,99 +14,65 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class DeathListener implements Listener {
+    private HashMap<UUID, ArrayList<PlayerKill>> recentKills = new HashMap<>();
+
     @EventHandler
     public void onDeath(PlayerDeathEvent e) {
-        int points = 0;
         if (e.getEntity().getKiller() != null) {
             Player killer = e.getEntity().getKiller();
             Player victim = e.getEntity();
             LeaderboardPlugin.getPlugin().getLogger().info("Killer: " + killer.getName() + " Victim: " + victim.getName());
             if (killer.getAddress().getAddress().equals(victim.getAddress().getAddress()))
                 return;
-            points += calcPoints(victim);
-            if (LeaderboardPlugin.playerPoints.containsKey(killer.getUniqueId())) {
-                for (PlayerKill playerKill : LeaderboardPlugin.playerPoints.get(killer.getUniqueId())) {
-                    if ((playerKill.getVictim().equals(victim.getUniqueId()) || playerKill.getVictimIP().equals(victim.getAddress().getAddress())) &&
-                            playerKill.getTime().longValue() > System.currentTimeMillis())
-                        return;
-                }
-                ArrayList<PlayerKill> playerPoints = (ArrayList<PlayerKill>)LeaderboardPlugin.playerPoints.get(killer.getUniqueId());
-                playerPoints.add(new PlayerKill(killer.getUniqueId(), victim.getUniqueId(), Long.valueOf(System.currentTimeMillis() + (3600000 * 1000 / points)), victim.getAddress().getAddress()));
-                LeaderboardPlugin.playerPoints.put(killer.getUniqueId(), playerPoints);
-                increaseKillCount(killer, points);
-            } else {
-                ArrayList<PlayerKill> playerPoints = new ArrayList<>();
-                playerPoints.add(new PlayerKill(killer.getUniqueId(), victim.getUniqueId(), Long.valueOf(System.currentTimeMillis() + (3600000 * 1000 / points)), victim.getAddress().getAddress()));
-                LeaderboardPlugin.playerPoints.put(killer.getUniqueId(), playerPoints);
-                increaseKillCount(killer, points);
-            }
+
+            if (!canEarnPoints(killer, victim))
+                return;
+
+            int points = calcPoints(victim);
+            ArrayList<PlayerKill> playerPoints = LeaderboardPlugin.playerPoints.getOrDefault(killer.getUniqueId(), new ArrayList<>());
+            playerPoints.add(new PlayerKill(killer.getUniqueId(), victim.getUniqueId(), Long.valueOf(System.currentTimeMillis() + (3600000 * 1000 / points)), victim.getAddress().getAddress()));
+            LeaderboardPlugin.playerPoints.put(killer.getUniqueId(), playerPoints);
+            increaseKillCount(killer, points);
+
             Component pointText = Component.text(points).color(TextColor.color(175354));
             Component text = ((TextComponent)Component.text(" §r(").append(pointText)).append((Component)Component.text("§r)"));
             e.deathMessage(e.deathMessage().append(text));
         }
     }
 
-    public void increaseKillCount(Player player, int points) {
-        if (LeaderboardPlugin.killCount.containsKey(player.getUniqueId())) {
-            LeaderboardPlugin.killCount.computeIfPresent(player.getUniqueId(), (k, v) -> Integer.valueOf(v.intValue() + points));
-        } else {
-            LeaderboardPlugin.killCount.put(player.getUniqueId(), Integer.valueOf(points));
+    private boolean canEarnPoints(Player killer, Player victim) {
+        long currentTime = System.currentTimeMillis();
+        ArrayList<PlayerKill> kills = recentKills.computeIfAbsent(killer.getUniqueId(), k -> new ArrayList<>());
+        kills.removeIf(k -> (currentTime - k.getTime()) > 21600000); // 6 hours in milliseconds
+
+        long count = kills.stream().filter(k -> k.getVictim().equals(victim.getUniqueId())).count();
+        if (count >= 3) {
+            killer.sendMessage(Component.text("You cannot earn points for killing " + victim.getName() + " more than three times in 6 hours.", NamedTextColor.RED));
+            return false;
         }
+
+        kills.add(new PlayerKill(killer.getUniqueId(), victim.getUniqueId(), currentTime, victim.getAddress().getAddress()));
+        return true;
     }
 
-    public boolean hasArmor(Player player) {
-        ItemStack[] armor = player.getInventory().getArmorContents();
-        for (ItemStack item : armor) {
-            if (item == null)
-                return false;
-            if (!item.containsEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL) && !item.containsEnchantment(Enchantment.PROTECTION_EXPLOSIONS))
-                return false;
-            if (item.getType().name().toLowerCase().contains("diamond")) {
-                if (item.containsEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL)) {
-                    if (item.getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL) < 4)
-                        return false;
-                } else if (item.containsEnchantment(Enchantment.PROTECTION_EXPLOSIONS) &&
-                        item.getEnchantmentLevel(Enchantment.PROTECTION_EXPLOSIONS) < 4) {
-                    return false;
-                }
-            } else if (item.getType().name().toLowerCase().contains("netherite")) {
-                if (item.containsEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL)) {
-                    if (item.getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL) < 3)
-                        return false;
-                } else if (item.containsEnchantment(Enchantment.PROTECTION_EXPLOSIONS) &&
-                        item.getEnchantmentLevel(Enchantment.PROTECTION_EXPLOSIONS) < 3) {
-                    return false;
-                }
-            }
-        }
-        return true;
+    public void increaseKillCount(Player player, int points) {
+        LeaderboardPlugin.killCount.merge(player.getUniqueId(), points, Integer::sum);
     }
 
     public static int calcPoints(Player player) {
         int points = 100;
-        ArrayList<ItemStack> armor = new ArrayList<>();
-        armor.add(player.getInventory().getHelmet());
-        armor.add(player.getInventory().getChestplate());
-        armor.add(player.getInventory().getLeggings());
-        armor.add(player.getInventory().getBoots());
+        ItemStack[] armor = player.getInventory().getArmorContents();
         for (ItemStack item : armor) {
-            if (item == null)
-                continue;
-            if (item.getType().name().toLowerCase().contains("diamond")) {
-                points += 30;
-            } else if (item.getType().name().toLowerCase().contains("netherite")) {
-                points += 50;
-            }
-            for (Enchantment enchantment : item.getEnchantments().keySet()) {
-                if (enchantment.equals(Enchantment.BINDING_CURSE) || enchantment.equals(Enchantment.VANISHING_CURSE))
-                    continue;
-                if (enchantment.equals(Enchantment.PROTECTION_ENVIRONMENTAL) || enchantment.equals(Enchantment.PROTECTION_EXPLOSIONS)) {
-                    points += item.getEnchantmentLevel(enchantment) * 30;
-                    continue;
+            if (item != null && (item.getType().name().toLowerCase().contains("diamond") || item.getType().name().toLowerCase().contains("netherite"))) {
+                points += (item.getType().name().toLowerCase().contains("diamond")) ? 30 : 50;
+                for (Enchantment enchantment : item.getEnchantments().keySet()) {
+                    int levelPoints = enchantment.equals(Enchantment.PROTECTION_ENVIRONMENTAL) || enchantment.equals(Enchantment.PROTECTION_EXPLOSIONS) ? 30 : 10;
+                    points += item.getEnchantmentLevel(enchantment) * levelPoints;
                 }
-                points += item.getEnchantmentLevel(enchantment) * 10;
             }
         }
         return points;
